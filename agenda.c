79d558e19499ce177dd0cd7c79e8511488cd46d0
VitorBorg <42881882+VitorBorg@users.noreply.github.com>
2019-08-30 11:26:48 -0300

    Update agenda.c

--- agenda.c
+++ agenda.c
@@ -3,6 +3,9 @@
 #include <string.h>
 
 void rep();
+int add();
+int rem();
+void list();
 
 typedef struct {
     char name[20];
@@ -23,10 +26,22 @@ int main(){
         printf("0 para adicionar, 1 para excluir, 2 para buscar, 3 para listar, 4 para sair e 5 para ordenar por idade.\n");
         scanf("%d", &(*option));
         
-        if(*option == 0)
-			printf("ADD");
-		else if(*option == 2)
-			printf("REMOVE");
+        if(*option == 0){
+			add();
+			printf("\nUsuario adicionado.");
+		}
+		else if(*option == 1){
+			rem();
+			printf("\nUsuario removido.");
+		}
+			else if(*option == 2){
+			rem();
+			printf("\nBusca finalizada.");
+		}
+			else if(*option == 3){
+			rem();
+			printf("\nUsuarios listados.");
+		}
         
     } while(*option != 4);
 
@@ -38,4 +53,33 @@ void rep(){
     option = pBuffer + (sizeof(int) * 2) + 1;
     temp = pBuffer + (sizeof(int) * 3) + 1;
     temporary = pBuffer + (sizeof(int) * 4) + 1;
+}
+
+int add(){
+	pBuffer = realloc(pBuffer, *size + (sizeof(user)));
+	rep();
+	
+	user *new = (user*)(pBuffer + *size + 1);
+	
+	*size = *size + sizeof(user);
+	*usersLen = *usersLen + 1;
+	
+	printf("\nInsira o seu nome: ");
+	scanf("%s", new->name); //NAO PRECISA DO & PORQUE UMA E UMA STRING
+	
+	printf("\nInsira a sua cidade: ");
+	scanf("%s", new->city);
+	
+	printf("\nInsira a sua idade: ");
+	scanf("%d", &new->age);
+	
+	return 1;
+}
+
+int rem(){
+	return 1;
+}
+
+void list(){
+	
 }
